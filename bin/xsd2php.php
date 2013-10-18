2cf8551bcfb5088f60f69ad91a931af5ae5fe58a
Jaap van Otterdijk <jaap@youngguns.nl>
2013-10-18 21:27:52 +0200

    fix autoloading of bin using composer

--- bin/xsd2php.php
+++ bin/xsd2php.php
@@ -1,21 +1,9 @@
 #!/bin/php
 <?php
 
-if($_ENV["SYMFONY_PATH"]){
-	$symfonyPath = $_ENV["SYMFONY_PATH"];
-}else{
-	$symfonyPath =  __DIR__.'/../lib';
-}
+include __DIR__ . '/../vendor/autoload.php';
 
-require_once $symfonyPath.'/Symfony/Component/ClassLoader/UniversalClassLoader.php'; 
+use Goetas\Xsd\XsdToPhp\Console\ConsoleRunner;
 
-$loader = new \Symfony\Component\ClassLoader\UniversalClassLoader();
-
-$loader->registerNamespaces(array(
-    'Symfony'          => $symfonyPath,
-	'Goetas\Xsd\XsdToPhp'      => __DIR__.'/../lib',
-));
-
-$loader->register();
-
-\Goetas\Xsd\XsdToPhp\Console\ConsoleRunner::run();
+$cli = new ConsoleRunner();
+$cli->run();
