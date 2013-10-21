c78984968815daebc2b5248f34fab161f6c8d87e
Asmir Mustafic <asmir@lignano.it>
2013-10-21 22:06:54 +0200

    Update ConsoleRunner.php

--- lib/Goetas/Xsd/XsdToPhp/Console/ConsoleRunner.php
+++ lib/Goetas/Xsd/XsdToPhp/Console/ConsoleRunner.php
@@ -14,22 +14,14 @@ class ConsoleRunner
      * @param  \Symfony\Component\Console\Command\Command[] $commands
      * @return void
      */
-    public static function run($commands = array())
+    public function run($commands = array())
     {
         $cli = new Application('Convert XSD to PHP classes Command Line Interface', "1.0");
         $cli->setCatchExceptions(true);
-        self::addCommands($cli);
-        $cli->addCommands($commands);
-        $cli->run();
-    }
-
-    /**
-     * @param Application $cli
-     */
-    public static function addCommands(Application $cli)
-    {
         $cli->addCommands(array(
             new \Goetas\Xsd\XsdToPhp\Command\Convert()
         ));
+        $cli->addCommands($commands);
+        $cli->run();
     }
 }
