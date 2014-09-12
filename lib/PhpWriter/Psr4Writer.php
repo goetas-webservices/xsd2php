292f093ea2887001aba7db5fea8d23f0892e99e8
agnagna <wis.agn@gmail.com>
2014-09-12 10:45:44 +0200

    Update Psr4Writer.php
    Removing white characters worked for me

--- lib/PhpWriter/Psr4Writer.php
+++ lib/PhpWriter/Psr4Writer.php
@@ -10,7 +10,7 @@ class Psr4Writer extends BasePsr4Writer implements ClassWriter
     public function write(PHPType $php, $content)
     {
         foreach ($this->namespaces as $namespace => $dir) {
-            if (strpos($php->getNamespace() . "\\", $namespace) === 0) {
+            if (strpos(trim($php->getNamespace()) . "\\", $namespace) === 0) {
                 $d = strtr(substr($php->getNamespace(), strlen($namespace)), "\\", "/");
                 $dir = rtrim($dir, "/") . "/" . $d;
                 if (! is_dir($dir) && ! mkdir($dir, 0777, true)) {
