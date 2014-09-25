f456c2fdfa36bc8b2b3342895ea4fbbe510250fd
Asmir Mustafic <goetas@gmail.com>
2014-09-25 09:38:09 +0200

    Notices not displayed

--- bin/xsd2php.php
+++ bin/xsd2php.php
@@ -13,7 +13,7 @@ foreach ($paths as $path) {
 
 use Symfony\Component\Console\Application;
 use Goetas\Xsd\XsdToPhp\Command;
-
+error_reporting(error_reporting() &~E_NOTICE);
 $cli = new Application('Convert XSD to PHP classes Command Line Interface', "2.0");
 $cli->setCatchExceptions(true);
 $cli->addCommands(array(
