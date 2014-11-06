0497db386183ec6dae4da13ac5b794cc784de714
Asmir Mustafic <goetas@gmail.com>
2014-11-06 19:11:52 +0100

    Test refactoring

--- tests/PHP/PHPConversionTest.php
+++ tests/PHP/PHPConversionTest.php
@@ -19,35 +19,57 @@ use Composer\Autoload\ClassLoader;
 class PHPConversionTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testMulteplicity()
+    /**
+     *
+     * @param mixed $xml
+     * @return \Zend\Code\Generator\ClassGenerator[]
+     */
+    protected function getClasses($xml)
     {
         $phpcreator = new PhpConverter();
         $phpcreator->addNamespace('http://www.example.com', 'Example');
 
+        $generator = new ClassGenerator();
         $reader = new SchemaReader();
-        $schemas = array(
-            $reader->readString('
-                <xs:schema targetNamespace="http://www.example.com"
-                xmlns:xs="http://www.w3.org/2001/XMLSchema">
-                    <xs:complexType name="single">
-                        <xs:all>
-                            <xs:element name="id" type="xs:long" minOccurs="0"/>
-                        </xs:all>
-                    </xs:complexType>
-                </xs:schema>
-                '));
 
+        if (! is_array($xml)) {
+            $xml = [
+                'schema.xsd' => $xml
+            ];
+        }
+        $schemas = [];
+        foreach ($xml as $name => $str) {
+            $schemas[] = $reader->readString($str, $name);
+        }
         $items = $phpcreator->convert($schemas);
 
-        $this->assertCount(1, $items);
-
-        $generator = new ClassGenerator();
-        $classGen = new \Zend\Code\Generator\ClassGenerator();
-        foreach ($items as $item) {
-            if ($generator->generate($classGen, $item)) {
-                $this->assertFalse($classGen->hasMethod('issetId'));
-                $this->assertFalse($classGen->hasMethod('unsetId'));
+        $classes = array();
+        foreach ($items as $k => $item) {
+            $codegen = new \Zend\Code\Generator\ClassGenerator();
+            if ($generator->generate($codegen, $item)) {
+                $classes[$k] = $codegen;
             }
         }
+        return $classes;
+    }
+
+    public function testMulteplicity()
+    {
+        $xml = '
+            <xs:schema targetNamespace="http://www.example.com"
+            xmlns:xs="http://www.w3.org/2001/XMLSchema">
+                <xs:complexType name="single">
+                    <xs:all>
+                        <xs:element name="id" type="xs:long" minOccurs="0"/>
+                    </xs:all>
+                </xs:complexType>
+            </xs:schema>';
+
+        $items = $this->getClasses($xml);
+        $this->assertCount(1, $items);
+
+        $codegen = $items['Example\SingleType'];
+        $this->assertFalse($codegen->hasMethod('issetId'));
+        $this->assertFalse($codegen->hasMethod('unsetId'));
     }
 }
