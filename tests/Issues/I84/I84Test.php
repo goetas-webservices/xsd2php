90fc4778bf912eabcee786b5f3ac1ed91e367c86
Asmir Mustafic <goetas@gmail.com>
2015-08-10 22:36:10 +0200

    Added tests for naming with dots
    Fixes #84

---
+++ tests/Issues/I84/I84Test.php
@@ -0,0 +1,21 @@
+<?php
+namespace Goetas\Xsd\XsdToPhp\Tests\Issues\I63;
+use Goetas\XML\XSDReader\SchemaReader;
+use Goetas\Xsd\XsdToPhp\Jms\YamlConverter;
+use Goetas\Xsd\XsdToPhp\Php\PhpConverter;
+use Goetas\Xsd\XsdToPhp\Naming\ShortNamingStrategy;
+
+class I84Test extends \PHPUnit_Framework_TestCase{
+
+    public function testNaming()
+    {
+        $reader = new SchemaReader();
+        $schema = $reader->readFile(__DIR__.'/data.xsd');
+
+        $phpConv = new PhpConverter(new ShortNamingStrategy());
+        $phpConv->addNamespace('http://www.example.com/', 'Epa');
+
+        $phpClasses = $phpConv->convert([$schema]);
+        $this->assertArrayHasKey('Epa\ABType', $phpClasses);
+    }
+}
