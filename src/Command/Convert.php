464ea03e86ebdf9bc9f5ade657387b98b34306f0
Asmir Mustafic <goetas@gmail.com>
2016-08-02 14:49:06 +0200

    improved command

--- src/Command/Convert.php
+++ src/Command/Convert.php
@@ -2,21 +2,22 @@
 namespace GoetasWebservices\Xsd\XsdToPhp\Command;
 
 use Exception;
-use GoetasWebservices\Xsd\XsdToPhp\Php\ClassWriter;
-use GoetasWebservices\Xsd\XsdToPhp\Php\PathGenerator\Psr4PathGenerator;
-use Symfony\Component\Console;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\Yaml\Dumper;
 
-class Convert extends Console\Command\Command
+class Convert extends Command
 {
     protected $container;
+    protected $what;
 
-    public function __construct(ContainerInterface $container)
+    public function __construct(ContainerInterface $container, $what)
     {
         $this->container = $container;
+        $this->what = $what;
         parent::__construct();
     }
 
@@ -26,10 +27,9 @@ class Convert extends Console\Command\Command
      */
     protected function configure()
     {
-        $this->setName('convert');
+        $this->setName('xsd2php:convert:' . $this->what);
 
         $this->setDefinition(array(
-            new InputArgument('format', InputArgument::REQUIRED, 'Format'),
             new InputArgument('src', InputArgument::REQUIRED | InputArgument::IS_ARRAY, 'Where is located your XSD definitions'),
             new InputOption('ns-map', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'How to map XML namespaces to PHP namespaces? Syntax: <info>XML-namespace;PHP-namespace</info>'),
             new InputOption('ns-dest', null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, 'Where place the generated files? Syntax: <info>PHP-namespace;destination-directory</info>'),
@@ -42,54 +42,22 @@ class Convert extends Console\Command\Command
      *
      * @see Console\Command\Command
      */
-    protected function execute(Console\Input\InputInterface $input, Console\Output\OutputInterface $output)
+    protected function execute(InputInterface $input, OutputInterface $output)
     {
         $this->container->set('logger', new \Symfony\Component\Console\Logger\ConsoleLogger($output));
         $naming = $this->container->get('goetas.xsd2php.naming_convention.' . $input->getOption('naming-strategy'));
         $this->container->set('goetas.xsd2php.naming_convention', $naming);
         $logger = $this->container->get('logger');
-        $pathGenerator = $this->container->get('goetas.xsd2php.path_generator.jms.psr4');
-        $this->container->set('goetas.xsd2php.path_generator.jms', $pathGenerator);
+        $pathGenerator = $this->container->get('goetas.xsd2php.path_generator.' . $this->what . '.psr4');
+        $this->container->set('goetas.xsd2php.path_generator.' . $this->what, $pathGenerator);
 
-        $pathGenerator = $this->container->get('goetas.xsd2php.path_generator.php.psr4');
-        $this->container->set('goetas.xsd2php.path_generator.php', $pathGenerator);
+        $converter = $this->container->get('goetas.xsd2php.converter.' . $this->what);
 
-        $format = strtr($input->getArgument('format'), '-', '_');
-        $converter = $this->container->get('goetas.xsd2php.converter.' . $format);
-
-        $nsMap = $input->getOption('ns-map');
-        if (!$nsMap) {
-            throw new \RuntimeException(__CLASS__ . ' requires at least one ns-map.');
+        foreach ($this->getMapOption($input, 'ns-map', 2, 1) as list($xmlNs, $phpNs)) {
+            $converter->addNamespace($xmlNs, $this->sanitizePhp($phpNs));
         }
-        foreach ($nsMap as $val) {
-            if (substr_count($val, ';') !== 1) {
-                throw new Exception('Invalid syntax for --ns-map');
-            }
-            list ($xmlNs, $phpNs) = explode(';', $val, 2);
-            $converter->addNamespace($xmlNs, trim(strtr($phpNs, './', '\\\\'), '\\'));
-        }
-
-        $nsTarget = $input->getOption('ns-dest');
-        if (!$nsTarget) {
-            throw new \RuntimeException(__CLASS__ . ' requires at least one ns-dest.');
-        }
-        $targets = array();
-        foreach ($nsTarget as $val) {
-            if (substr_count($val, ';') !== 1) {
-                throw new Exception('Invalid syntax for --ns-dest');
-            }
-            list ($phpNs, $dir) = explode(';', $val, 2);
-            $phpNs = strtr($phpNs, './', '\\\\');
-
-            $targets[$phpNs] = $dir;
-        }
-        $arrayMap = $input->getOption('alias-map');
-        foreach ($arrayMap as $val) {
-            if (substr_count($val, ';') !== 2) {
-                throw new Exception('Invalid syntax for --alias-map');
-            }
-            list ($xmlNs, $name, $type) = explode(';', $val, 3);
-            $converter->addAliasMapType($xmlNs, $name, $type);
+        foreach ($this->getMapOption($input, 'alias-map', 3, 0) as list($xmlNs, $name, $phpNs)) {
+            $converter->addAliasMapType($xmlNs, $name, $this->sanitizePhp($phpNs));
         }
 
         $src = $input->getArgument('src');
@@ -102,13 +70,38 @@ class Convert extends Console\Command\Command
         }
         $items = $converter->convert($schemas);
 
-        $pathGenerator = $this->container->get('goetas.xsd2php.path_generator.' . $format . '.psr4');
+        $pathGenerator = $this->container->get('goetas.xsd2php.path_generator.' . $this->what . '.psr4');
+
+        $targets = [];
+        foreach ($this->getMapOption($input, 'ns-dest', 2, 1) as list($phpNs, $path)) {
+            $targets[$this->sanitizePhp($phpNs)] = $path;
+        }
         $pathGenerator->setTargets($targets);
 
-        $writer = $this->container->get('goetas.xsd2php.writer.' . $format);
+        $writer = $this->container->get('goetas.xsd2php.writer.' . $this->what);
         $writer->write($items);
         $logger->info(sprintf('Writing %s items', count($items)));
 
-        return 0;
+        return count($items) ? 0 : 255;
+    }
+
+    protected function getMapOption(InputInterface $input, $nsMapName, $miSplit = 2, $minRep = 0)
+    {
+        $nsMap = $input->getOption($nsMapName);
+        if (count($nsMap) < $minRep) {
+            throw new \RuntimeException(__CLASS__ . ' requires at least one ' . $nsMapName . '.');
+        }
+        return array_map(function ($val) use ($miSplit, $nsMapName) {
+            if (substr_count($val, ';') !== ($miSplit - 1)) {
+                throw new Exception('Invalid syntax for --' . $nsMapName);
+            }
+            return explode(';', $val, $miSplit);
+
+        }, $nsMap);
+    }
+
+    protected function sanitizePhp($ns)
+    {
+        return strtr($ns, '/', '\\');
     }
 }
