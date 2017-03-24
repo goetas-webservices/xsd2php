3c57b2ed1d3af9578b3ea86e286c5f11f44fc0a0
Jeroen van der Geer <jeroen@swis.nl>
2017-03-24 15:08:32 +0100

    Add "skip_when_empty" to xml_list in the YAML.

---
+++ tests/Issues/I26/I26Test.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace GoetasWebservices\Xsd\XsdToPhp\Tests\Issues\I26;
+
+use GoetasWebservices\XML\XSDReader\SchemaReader;
+use GoetasWebservices\Xsd\XsdToPhp\Jms\YamlConverter;
+use GoetasWebservices\Xsd\XsdToPhp\Naming\ShortNamingStrategy;
+
+class I26Test extends \PHPUnit_Framework_TestCase
+{
+    public function testSkipWhenEmptyOnOptionalAndRequiredLists()
+    {
+        $reader = new SchemaReader();
+        $schema = $reader->readFile(__DIR__.'/data.xsd');
+
+        $yamlConverter = new YamlConverter(new ShortNamingStrategy());
+        $yamlConverter->addNamespace('', 'NestedArrayTest');
+        $phpClasses = $yamlConverter->convert([$schema]);
+
+        $mainElement = $phpClasses['NestedArrayTest\MainElementType']['NestedArrayTest\MainElementType'];
+        $required = $mainElement['properties']['requiredElementList'];
+        $optional = $mainElement['properties']['optionalElementList'];
+
+        self::assertEquals(false, $required['xml_list']['skip_when_empty']);
+        self::assertEquals(true, $optional['xml_list']['skip_when_empty']);
+    }
+
+}
