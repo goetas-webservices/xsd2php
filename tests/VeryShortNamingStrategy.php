a895f3cc78997752ed3db4393ffa2cdf4f26d5eb
rtek <rtek@ryank.net>
2018-04-15 10:37:09 -0400

    cs fixes

--- tests/VeryShortNamingStrategy.php
+++ tests/VeryShortNamingStrategy.php
@@ -12,33 +12,40 @@ use GoetasWebservices\Xsd\XsdToPhp\Naming\ShortNamingStrategy;
 class VeryShortNamingStrategy extends ShortNamingStrategy
 {
     /**
-     * Suffix with T instead of Type
+     * Suffix with 'T' instead of 'Type'
      * @param Type $type
      * @return string
      */
     public function getTypeName(Type $type)
     {
-        if ($name = $this->classify($type->getName())) {
-            if (substr($name, -4) !== 'Type') {
-                $name .= "T";
-            } elseif (substr($name, -4) === 'Type') {
-                $name = substr($name, 0, -3);
-            }
+        $name = $this->classify($type->getName());
+
+        if ($name && substr($name, -4) !== 'Type') {
+            return $name . 'T';
         }
+
+        if (substr($name, -4) === 'Type') {
+            return substr($name, 0, -3);
+        }
+
         return $name;
     }
 
     /**
-     * Suffix with A instead of AType
+     * Suffix with 'A' instead of 'AType'
      * @param Type $type
      * @param string $parentName
      * @return string
      */
     public function getAnonymousTypeName(Type $type, $parentName)
     {
-        return $this->classify($parentName) . "A";
+        return $this->classify($parentName) . 'A';
     }
 
+    /**
+     * @param string $name
+     * @return string
+     */
     private function classify($name)
     {
         return Inflector::classify(str_replace(".", " ", $name));
