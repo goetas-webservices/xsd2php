94edec2e7c1011b1673eafe39297cbfa98a7b11d
Asmir Mustafic <goetas@gmail.com>
2021-01-23 16:24:46 +0100

    add NoConflictLongNamingStrategy

---
+++ src/Naming/AbstractNamingStrategy.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace GoetasWebservices\Xsd\XsdToPhp\Naming;
+
+use Doctrine\Common\Inflector\Inflector;
+use GoetasWebservices\XML\XSDReader\Schema\Item;
+use GoetasWebservices\XML\XSDReader\Schema\Type\Type;
+
+abstract class AbstractNamingStrategy implements NamingStrategy
+{
+    protected $reservedWords = [
+        '__halt_compiler',
+        'abstract',
+        'and',
+        'array',
+        'as',
+        'bool',
+        'break',
+        'callable',
+        'case',
+        'catch',
+        'class',
+        'clone',
+        'const',
+        'continue',
+        'declare',
+        'default',
+        'die',
+        'do',
+        'echo',
+        'else',
+        'elseif',
+        'empty',
+        'enddeclare',
+        'endfor',
+        'endforeach',
+        'endif',
+        'endswitch',
+        'endwhile',
+        'eval',
+        'exit',
+        'extends',
+        'false',
+        'final',
+        'float',
+        'for',
+        'foreach',
+        'function',
+        'global',
+        'goto',
+        'if',
+        'implements',
+        'include',
+        'include_once',
+        'instanceof',
+        'insteadof',
+        'int',
+        'interface',
+        'isset',
+        'list',
+        'mixed',
+        'namespace',
+        'new',
+        'null',
+        'numeric',
+        'object',
+        'or',
+        'print',
+        'private',
+        'protected',
+        'public',
+        'require',
+        'require_once',
+        'resource',
+        'return',
+        'static',
+        'string',
+        'switch',
+        'throw',
+        'trait',
+        'true',
+        'try',
+        'unset',
+        'use',
+        'var',
+        'while',
+        'xor',
+    ];
+
+    public function getItemName(Item $item)
+    {
+        $name = $this->classify($item->getName());
+        if (in_array(strtolower($name), $this->reservedWords)) {
+            $name .= 'Xsd';
+        }
+
+        return $name;
+    }
+
+    public function getPropertyName($item)
+    {
+        return Inflector::camelize(str_replace('.', ' ', $item->getName()));
+    }
+
+    protected function classify($name)
+    {
+        return Inflector::classify(str_replace('.', ' ', $name));
+    }
+}
--- src/Naming/ShortNamingStrategy.php
+++ src/Naming/ShortNamingStrategy.php
@@ -6,87 +6,8 @@ use Doctrine\Common\Inflector\Inflector;
 use GoetasWebservices\XML\XSDReader\Schema\Item;
 use GoetasWebservices\XML\XSDReader\Schema\Type\Type;
 
-class ShortNamingStrategy implements NamingStrategy
+class ShortNamingStrategy extends AbstractNamingStrategy
 {
-    protected $reservedWords = [
-        '__halt_compiler',
-        'abstract',
-        'and',
-        'array',
-        'as',
-        'bool',
-        'break',
-        'callable',
-        'case',
-        'catch',
-        'class',
-        'clone',
-        'const',
-        'continue',
-        'declare',
-        'default',
-        'die',
-        'do',
-        'echo',
-        'else',
-        'elseif',
-        'empty',
-        'enddeclare',
-        'endfor',
-        'endforeach',
-        'endif',
-        'endswitch',
-        'endwhile',
-        'eval',
-        'exit',
-        'extends',
-        'false',
-        'final',
-        'float',
-        'for',
-        'foreach',
-        'function',
-        'global',
-        'goto',
-        'if',
-        'implements',
-        'include',
-        'include_once',
-        'instanceof',
-        'insteadof',
-        'int',
-        'interface',
-        'isset',
-        'list',
-        'mixed',
-        'namespace',
-        'new',
-        'null',
-        'numeric',
-        'object',
-        'or',
-        'print',
-        'private',
-        'protected',
-        'public',
-        'require',
-        'require_once',
-        'resource',
-        'return',
-        'static',
-        'string',
-        'switch',
-        'throw',
-        'trait',
-        'true',
-        'try',
-        'unset',
-        'use',
-        'var',
-        'while',
-        'xor',
-    ];
-
     public function getTypeName(Type $type)
     {
         $name = $this->classify($type->getName());
@@ -100,25 +21,5 @@ class ShortNamingStrategy implements NamingStrategy
     public function getAnonymousTypeName(Type $type, $parentName)
     {
         return $this->classify($parentName) . 'AType';
-    }
-
-    public function getPropertyName($item)
-    {
-        return Inflector::camelize(str_replace('.', ' ', $item->getName()));
-    }
-
-    public function getItemName(Item $item)
-    {
-        $name = $this->classify($item->getName());
-        if (in_array(strtolower($name), $this->reservedWords)) {
-            $name .= 'Xsd';
-        }
-
-        return $name;
-    }
-
-    private function classify($name)
-    {
-        return Inflector::classify(str_replace('.', ' ', $name));
     }
 }
